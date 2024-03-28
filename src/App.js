b218cf52c2802435bf4d95dab693a5bce2418de2
AniAntonyan <94914782+AniAntonyan@users.noreply.github.com>
2024-03-28 07:46:23 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [boards, setBoards] = useState([
+    {id: 1, title: 'Start', items: [{id: 1, title: 'React'}, {id: 2, title: 'Vue'}, {id: 3, title: 'Angular'}]},
+    {id: 2, title: 'In Process', items: []},
+    {id: 3, title: 'Finished', items: []},
+])
+
+const addItem = ()=>{
+    const item = {id: Date.now(), title: input};
+    let newItem = [item, ...boards[0]['items']];
+
+    setBoards( boards.map( board => {
+        if(board.id == 1) board.items = newItem;
+        return board;
+    }))
 }
 
-export default App;
+const submit = (e)=>{
+    e.preventDefault();
+    addItem(input);
+    setInput('');
+}
+
+const [currentBoard, setCurrentBoard] = useState(null);
+const [currentItem, setCurrentItem] = useState(null);
+const [input, setInput] = useState('');
+
+const dragOverFunc = (e)=>{
+    e.preventDefault();
+}
+
+const dragStartFunc = (e, board, item)=>{
+    setCurrentBoard(board);
+    setCurrentItem(item);
+}
+
+const dropEndFunc = (e, board) => {
+    board.items.push(currentItem);
+
+    const index = currentBoard.items.indexOf(currentItem);
+    currentBoard.items.splice(index, 1);
+
+    setBoards( boards.map (b => {
+        if(b.id == board.id) return board;
+        if(b.id == currentBoard.id) return currentBoard;
+        return b;
+    }));
+}
+
+const removeItem = (board, item)=>{
+    const index = board.items.indexOf(item);
+    board.items.splice(index, 1);
+
+    setBoards( boards.map (b => {
+        if(b.id == board.id) return board;
+        return b
+    }));
+}
+
+return(
+    <div className="App">
+        <form onSubmit={submit}>
+            <input 
+            type="text" 
+            placeholder="Your todo..."
+            value={input}
+            onChange={e=>setInput(e.target.value)}
+            />
+        </form>
+
+        {
+            boards.map( board => 
+            <div 
+              className="board"
+              onDragOver={e=>dragOverFunc(e)}
+              onDrop={e=>dropEndFunc(e, board)}
+              key={board.id}
+            >
+                <h1>{board.title}</h1>
+                {
+                    board.items.map( item =>  
+                        <div 
+                          className="item"
+                          draggable={true}
+                          onDragOver={e=>dragOverFunc(e)}
+                          onDragStart={e=>dragStartFunc(e, board, item)}
+                          key={item.id}
+                        >
+                            <span>{item.title}</span>
+
+                            <span onClick={()=>removeItem(board, item)} className="x">
+                              x
+                            </span>
+
+                        </div>
+
+                    )
+                }
+            </div> 
+            )
+        }
+    </div>
+)
+
+}
